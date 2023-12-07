7251e011964b23287d7ac7b6ae53c1a4ad206c9c
salome <sallygithinji72@gmail.com>
2023-12-07 23:16:46 +0300

    connecting to datatbase

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app =  express()
 
+const db = mysql.createConnector({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"chasing_book"
+}
+
+)
+ 
 app.listen (8800,()=> {
     console.log("connected to Backend")
 
