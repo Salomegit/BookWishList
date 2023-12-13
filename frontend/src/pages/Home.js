19a07150af127c0091ed7ba4c7efffa33732b13b
salome <sallygithinji72@gmail.com>
2023-12-13 17:30:34 +0300

    using link tags

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <p>Home pae</p>
+   <Link to="/add">Add book</Link>
     </div>
   )
 }
