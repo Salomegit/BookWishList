50eedc9ffd2cd8a6e6466cdaf3fc90609a8f6d13
salome <sallygithinji72@gmail.com>
2023-12-19 00:13:16 +0300

    useState

--- frontend/src/pages/Addbook.js
+++ frontend/src/pages/Addbook.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Addbook = () => {
+
+  const [book,setBook] = useState({
+    book_title: "",
+    decscription : "",
+    cover:"",
+    price: null
+
+  })
+
+  const onChangebook = (e) =>{
+    setBook(prev=> ({...prev,[e.target.name] : e.target.value}))
+  }
+
+  console.log(book)
   return (
     <div className='form'>
         <p>Add book</p>
-        <input type='text' placeholder='Book Title'/>
+        <input type='text' placeholder='Book Title' onChange={onChangebook} name="book_title"/>
+        <input type='text' placeholder='Book Description' onChange={onChangebook}  name="decscription"/>
+        <input type='text' placeholder='Book Price' onChange={onChangebook}  name="price"/>
+        <input type='text' placeholder='Book Cover' onChange={onChangebook}  name='cover'/>
+
     </div>
   )
 }
